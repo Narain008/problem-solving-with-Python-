1bfaa38811d07230d7f76c1cb9a3b3fe37054dd2
Narain008 <narain8567.beaift25@chitkara.edu.in>
2025-09-16 23:42:55 +0530

    Add files via upload

---
+++ how to print even no in list 50.py
@@ -0,0 +1,3 @@
+# QUESTION---Write a Python program to extract even numbers from a list using list comprehension
+even=[x%2==0 for x in range(1,11)]
+print(even)
---
+++ how to print square using list comprehension 50.py
@@ -0,0 +1,3 @@
+# QUESTION---Write a Python program to generate squares of first 10 numbers using list comprehension.
+square=[x*x for x in range(1,11)]
+print(square)
